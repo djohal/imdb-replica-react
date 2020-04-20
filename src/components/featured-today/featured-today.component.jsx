e9b0404cede3386e14108e24cb58fe501d462a1e
djohal <dilpreet.johal022@gmail.com>
2020-04-20 13:40:50 -0400

    wrap up featured today component

--- src/components/featured-today/featured-today.component.jsx
+++ src/components/featured-today/featured-today.component.jsx
@@ -51,31 +51,33 @@ const FeaturedToday = ({ fetchFeaturedTodayStart, collections }) => {
           arrows
           autoPlaySpeed={2000}
           centerMode={false}
-          className=""
-          dotListClass=""
           draggable
           focusOnSelect={false}
           infinite={false}
-          itemClass=""
           keyBoardControl
           minimumTouchDrag={80}
           renderButtonGroupOutside={false}
           renderDotsOutside={false}
           responsive={responsive}
           showDots={false}
-          sliderClass=""
           slidesToSlide={2}
           swipeable
         >
           {collections.map(
-            ({ backdrop_path, title, vote_average, poster_path, id }) => (
-              <div className="carousel-images">
-                <img
-                  className="d-block carousel-img"
-                  src={`https://image.tmdb.org/t/p/w500/${backdrop_path}`}
-                  alt={title}
-                />
-              </div>
+            ({ backdrop_path, title, name, release_date, first_air_date, id }) => (
+              <React.Fragment key={id}>
+                <div className="carousel-images">
+                  <img
+                    className="d-block carousel-img"
+                    src={`https://image.tmdb.org/t/p/w500/${backdrop_path}`}
+                    alt={title}
+                  />
+                </div>
+                <div className="featured-details">
+                  <span>{title || name}</span>
+                  <span>{release_date || first_air_date}</span>
+                </div>
+              </React.Fragment>
             )
           )}
         </Carousel>
