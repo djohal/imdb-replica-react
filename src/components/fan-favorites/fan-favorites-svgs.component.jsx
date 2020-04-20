18cccf6d7a0729ddac39984220f06957a5ba4037
djohal <dilpreet.johal022@gmail.com>
2020-04-20 17:57:08 -0400

    finished styling for fan-favorites component

--- src/components/fan-favorites/fan-favorites-svgs.component.jsx
+++ src/components/fan-favorites/fan-favorites-svgs.component.jsx
@@ -39,3 +39,17 @@ export const WatchlistRibbonIconSvg = () => (
     <path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"></path>
   </svg>
 );
+
+export const RatingSvg = () => (
+  <svg
+    width="24"
+    height="24"
+    xmlns="http://www.w3.org/2000/svg"
+    class="ipc-icon ipc-icon--star-inline"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    role="presentation"
+  >
+    <path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path>
+  </svg>
+);
--- src/components/fan-favorites/fan-favorites.component.jsx
+++ src/components/fan-favorites/fan-favorites.component.jsx
@@ -9,8 +9,13 @@ import { selectMoviesCollection } from "../../redux/movies/selectors/fan-favorit
 import {
   WatchlistRibbonSvg,
   WatchlistRibbonIconSvg,
+  RatingSvg,
 } from "./fan-favorites-svgs.component";
 import CarouselContainer from "../carousel/carousel.component";
+
+const getSingleDecimal = (num) => {
+  return (Math.round(num * 10) / 10).toFixed(1);
+};
 
 const FanFavourites = ({ fetchFanFavoritesStart, collections }) => {
   useEffect(() => {
@@ -26,41 +31,50 @@ const FanFavourites = ({ fetchFanFavoritesStart, collections }) => {
           This week's most popular fan movies
         </span>
         <CarouselContainer desktop={6} slidesToSlide={6}>
-          {collections.map(
-            ({
-              poster_path,
-              title,
-              name,
-              release_date,
-              first_air_date,
-              id,
-            }) => (
-              <React.Fragment key={id}>
-                <div className="carousel-images">
-                  <img
-                    className="d-block carousel-img"
-                    src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
-                    alt={title}
-                  />
+          {collections.map(({ poster_path, title, name, vote_average, id }) => (
+            <React.Fragment key={id}>
+              <div className="carousel-images">
+                <img
+                  className="d-block carousel-img"
+                  src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+                  alt={title}
+                />
+              </div>
+              <div
+                className="watchlist-ribbon"
+                aria-label="add to watchlist"
+                role="button"
+                tabIndex="0"
+              >
+                <WatchlistRibbonSvg />
+                <div className="watchlist-ribbon__icon" role="presentation">
+                  <WatchlistRibbonIconSvg />
                 </div>
-                <div
-                  className="watchlist-ribbon"
-                  aria-label="add to watchlist"
-                  role="button"
-                  tabIndex="0"
-                >
-                  <WatchlistRibbonSvg />
-                  <div className="watchlist-ribbon__icon" role="presentation">
-                    <WatchlistRibbonIconSvg />
-                  </div>
+              </div>
+              <div className="featured-details">
+                <div className="rating">
+                  <RatingSvg />
+                  <span>{getSingleDecimal(vote_average)}</span>
                 </div>
-                <div className="featured-details">
-                  <span>{title || name}</span>
-                  <span>{release_date || first_air_date}</span>
-                </div>
-              </React.Fragment>
-            )
-          )}
+                <span>{title || name}</span>
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    class="ipc-icon ipc-icon--add ipc-button__icon ipc-button__icon--pre"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    role="presentation"
+                  >
+                    <path fill="none" d="M0 0h24v24H0V0z"></path>
+                    <path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"></path>
+                  </svg>
+                  <div class="button-text">Watchlist</div>
+                </button>
+              </div>
+            </React.Fragment>
+          ))}
         </CarouselContainer>
       </Container>
     </div>
