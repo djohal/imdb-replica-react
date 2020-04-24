e38dab8be9beebc69979293e58c5598189bb153a
djohal <dilpreet.johal022@gmail.com>
2020-04-24 07:43:26 -0400

    converted search class component to a functional component

--- src/components/search/search.component.jsx
+++ src/components/search/search.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
@@ -27,95 +27,87 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-class Search extends React.Component {
-  componentDidMount() {
-    const {
-      clearSearchEntry,
-      clearSearchCollections,
-      expandSearchInput,
-    } = this.props;
+const Search = ({
+  collections,
+  searchEntry,
+  expandSearchInput,
+  isSearchExpanded,
+  fetchSearchMovieStart,
+  searchInput,
+  clearSearchEntry,
+  clearSearchCollections,
+}) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const clearSearchData = useCallback(() => {
     clearSearchEntry();
     clearSearchCollections();
-    expandSearchInput(false);
-  }
+  }, [clearSearchEntry, clearSearchCollections]);
 
-  handleChange = (event) => {
-    const {
-      searchInput,
-      fetchSearchMovieStart,
-      clearSearchEntry,
-      clearSearchCollections,
-    } = this.props;
-    if (event.target.value.trim() !== "") {
-      searchInput(event.target.value);
+  useEffect(() => {
+    if (searchQuery.length > 0) {
+      searchInput(searchQuery);
       fetchSearchMovieStart();
     } else {
-      clearSearchEntry();
-      clearSearchCollections();
+      clearSearchData();
+      expandSearchInput(false);
     }
-  };
+  }, [
+    searchQuery,
+    searchInput,
+    fetchSearchMovieStart,
+    expandSearchInput,
+    clearSearchData,
+  ]);
 
-  clearSearchData = () => {
-    const { clearSearchEntry, clearSearchCollections } = this.props;
-    clearSearchEntry();
-    clearSearchCollections();
-  };
-  render() {
-    const {
-      collections,
-      searchEntry,
-      expandSearchInput,
-      isSearchExpanded,
-    } = this.props;
-    return (
-      <Form inline>
-        <div className="search-category-selector">
-          <span>All</span>
-          <DropDownIconSvg />
-        </div>
-        <div className="search-input-container">
-          <FormControl
-            type="text"
-            placeholder="Search IMDb"
-            onChange={(e) => this.handleChange(e)}
-            className={`${isSearchExpanded ? "search-sm" : null}`}
-          />
-          <FontAwesomeIcon
-            icon={faTimes}
-            color="white"
-            size="1x"
-            className={`${
-              isSearchExpanded
-                ? "cancel-search-expanded cancel-search"
-                : "cancel-search"
-            }`}
-            onClick={() => {
-              this.clearSearchData();
-              expandSearchInput(false);
-            }}
-          />
-        </div>
+  return (
+    <Form inline>
+      <div className="search-category-selector">
+        <span>All</span>
+        <DropDownIconSvg />
+      </div>
+      <div className="search-input-container">
+        <FormControl
+          type="text"
+          placeholder="Search IMDb"
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className={`${isSearchExpanded ? "search-sm" : null}`}
+        />
+        <FontAwesomeIcon
+          icon={faTimes}
+          color="white"
+          size="1x"
+          className={`${
+            isSearchExpanded
+              ? "cancel-search-expanded cancel-search"
+              : "cancel-search"
+          }`}
+          onClick={() => {
+            clearSearchData();
+            expandSearchInput(false);
+          }}
+        />
+      </div>
 
-        <div className="search-btn-container">
-          <button
-            type="button"
-            className={`${
-              isSearchExpanded ? "display-none search-button" : "search-button"
-            }`}
-            onClick={() => expandSearchInput(true)}
-          >
-            <SearchButtonSvg />
-          </button>
-        </div>
-        <div
-          className={`${searchEntry && !isSearchExpanded ? "overlay" : null}`}
-          onClick={() => this.clearSearchData()}
-        ></div>
-        <SearchDropdown collections={collections} searchEntry={searchEntry} />
-      </Form>
-    );
-  }
-}
+      <div className="search-btn-container">
+        <button
+          type="button"
+          className={`${
+            isSearchExpanded ? "display-none search-button" : "search-button"
+          }`}
+          onClick={() => expandSearchInput(true)}
+        >
+          <SearchButtonSvg />
+        </button>
+      </div>
+      <div
+        className={`${searchEntry && !isSearchExpanded ? "overlay" : null}`}
+        onClick={() => clearSearchData()}
+      ></div>
+      <SearchDropdown collections={collections} searchEntry={searchEntry} />
+    </Form>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   searchInput: (input) => dispatch(searchInput(input)),
