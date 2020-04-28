ecaea254e6eea980dfa0b8c4f9c32b247ced393c
djohal <dilpreet.johal022@gmail.com>
2020-04-28 15:23:59 -0400

    split header component into navbar and nav component

--- src/components/layout/header/header.component.jsx
+++ src/components/layout/header/header.component.jsx
@@ -1,27 +1,11 @@
 import React from "react";
-import { connect } from "react-redux";
-import { withRouter, Link, useRouteMatch } from "react-router-dom";
-import { createStructuredSelector } from "reselect";
+import { useRouteMatch } from "react-router-dom";
 
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-
-import Logo from "../../../static/assets/logo.png";
-import {
-  HamburgerMenuSvg,
-  ImdbProSvg,
-  WatchlistSvg,
-} from "./header-svgs.component";
-import SideNavContainer from "../../sidenav/sidenav.component";
-import SearchForm from "../../search-form/search-form.component";
-
-import { selectIsSearchExpanded } from "../../../redux/search/search.selectors";
-import { selectCurrentUser } from "../../../redux/user/user.selectors";
-import { signOutStart } from "../../../redux/user/user.actions";
 import { useWindowSize } from "../../../redux/movies/movies.utils";
 
-const Header = ({ isSearchExpanded, history, currentUser, signOutStart }) => {
+import NavBar from "./navbar/navbar.component";
+
+const Header = () => {
   const [windowWidth] = useWindowSize();
 
   let signUpUrlMatch = useRouteMatch("/sign-up");
@@ -37,65 +21,9 @@ const Header = ({ isSearchExpanded, history, currentUser, signOutStart }) => {
           : "header"
       }`}
     >
-      <Navbar
-        className={`${
-          isSearchExpanded ? "navbar-search-sm navbar-custom" : "navbar-custom"
-        }`}
-        variant="dark"
-      >
-        <Container>
-          <SideNavContainer isSearchExpanded={isSearchExpanded} />
-          <Navbar.Brand>
-            <img
-              className={`${isSearchExpanded ? "display-none logo" : "logo"}`}
-              src={Logo}
-              alt="logo"
-              onClick={() => history.push("/")}
-            />
-          </Navbar.Brand>
-          <Nav>
-            <Link to="" className="hide-responsive">
-              <HamburgerMenuSvg />
-              <span>Menu</span>
-            </Link>
-            <SearchForm />
-            <Link to="" className="hide-responsive">
-              <ImdbProSvg />
-            </Link>
-            <div className="verticle-line hide-responsive"></div>
-            <Link to="/watchlist" className="watchlist hide-responsive">
-              <WatchlistSvg />
-              <span>Watchlist</span>
-            </Link>
-            {currentUser ? (
-              <div
-                className={`${isSearchExpanded ? "display-none" : "sign-out"}`}
-                onClick={() => signOutStart()}
-              >
-                <span>Sign Out</span>
-              </div>
-            ) : (
-              <Link
-                to="/register/sign-in"
-                className={`${isSearchExpanded ? "display-none" : null}`}
-              >
-                <span>Sign In</span>
-              </Link>
-            )}
-          </Nav>
-        </Container>
-      </Navbar>
+      <NavBar />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  isSearchExpanded: selectIsSearchExpanded,
-  currentUser: selectCurrentUser,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  signOutStart: () => dispatch(signOutStart()),
-});
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Header));
+export default Header;
---
+++ src/components/layout/header/navbar/nav/nav.component.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Nav from "react-bootstrap/Nav";
+import {
+  HamburgerMenuSvg,
+  ImdbProSvg,
+  WatchlistSvg,
+} from "../../header-svgs.component";
+
+import SearchForm from "../../../../search-form/search-form.component";
+
+const NavLinks = ({ currentUser, signOutStart, isSearchExpanded }) => (
+  <Nav>
+    <Link to="" className="hide-responsive">
+      <HamburgerMenuSvg />
+      <span>Menu</span>
+    </Link>
+    <SearchForm />
+    <Link to="" className="hide-responsive">
+      <ImdbProSvg />
+    </Link>
+    <div className="verticle-line hide-responsive"></div>
+    <Link to="/watchlist" className="watchlist hide-responsive">
+      <WatchlistSvg />
+      <span>Watchlist</span>
+      <span>1</span>
+    </Link>
+    {currentUser ? (
+      <div
+        className={`${isSearchExpanded ? "display-none" : "sign-out"}`}
+        onClick={() => signOutStart()}
+      >
+        <span>Sign Out</span>
+      </div>
+    ) : (
+      <Link
+        to="/register/sign-in"
+        className={`${isSearchExpanded ? "display-none" : null}`}
+      >
+        <span>Sign In</span>
+      </Link>
+    )}
+  </Nav>
+);
+
+export default NavLinks;
