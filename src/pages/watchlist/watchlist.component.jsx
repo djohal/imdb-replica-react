e3554601ac0329d9e29fdbb9293baf2f31f68605
djohal <dilpreet.johal022@gmail.com>
2020-04-29 08:37:10 -0400

    remove item from watchlist page and redirect to homepage when no items left

--- src/pages/watchlist/watchlist.component.jsx
+++ src/pages/watchlist/watchlist.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
 import Container from "react-bootstrap/Container";
 
 import WatchlistItem from "../../components/watchlist-item/watchlist-item.component";
@@ -7,15 +9,18 @@ import { selectWatchlistItems } from "../../redux/watchlist/watchlist.selectors"
 
 const WatchlistPage = () => {
   const watchlistItems = useSelector(selectWatchlistItems);
+  const history = useHistory();
 
   return (
     <div className="watchlist-page">
       <Container>
         <h2>Your Watchlist</h2>
         <div className="watchlist-items">
-          {watchlistItems.map((item, i) => (
-            <WatchlistItem key={i} item={item} />
-          ))}
+          {watchlistItems.length
+            ? watchlistItems.map((item, i) => (
+                <WatchlistItem key={i} item={item} />
+              ))
+            : history.push("/")}
         </div>
       </Container>
     </div>
