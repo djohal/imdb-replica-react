3066c3fc9dcd2d2475531dba13084c602747647f
djohal <dilpreet.johal022@gmail.com>
2020-05-01 14:24:31 -0400

    pull user info on the account form

--- src/components/account-form/account-form.component.jsx
+++ src/components/account-form/account-form.component.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { selectCurrentUser } from "redux/user/user.selectors";
 
 const AccountForm = () => {
+  const { displayName, email } = useSelector(selectCurrentUser);
   return (
     <Form noValidate>
       <Form.Group controlId="formBasicName">
         <div className="form-info">
           <Form.Label>Name:</Form.Label>
-          <Form.Control plaintext readOnly defaultValue="Your Name" />
+          <Form.Control plaintext readOnly defaultValue={displayName} />
         </div>
         <Button type="button">Edit</Button>
       </Form.Group>
       <Form.Group controlId="formBasicEmail">
         <div className="form-info">
           <Form.Label>Email:</Form.Label>
-          <Form.Control plaintext readOnly defaultValue="email@example.com" />
+          <div className="email">{email}</div>
         </div>
         <Button type="button">Edit</Button>
       </Form.Group>
--- src/pages/account/account.component.jsx
+++ src/pages/account/account.component.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 
 import Logo from "static/assets/logo.png";
 import AccountForm from "../../components/account-form/account-form.component";
-import { useWindowSize } from "redux/movies/movies.utils";
 
 const AccountsPage = () => {
-  const [windowWidth] = useWindowSize();
 
   return (
     <div className="account">
