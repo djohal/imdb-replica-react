37c7bffc19841e52aec08e9555418e1f27a12338
djohal <dilpreet.johal022@gmail.com>
2020-05-02 09:20:31 -0400

    broke edit account form into multiple files

--- src/components/edit-account/edit-account-form.component.jsx
+++ src/components/edit-account/edit-account-form.component.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import { toast } from "react-toastify";
 
 import { selectCurrentUser } from "redux/user/user.selectors";
@@ -14,6 +12,9 @@ import {
   updateUserPassword,
 } from "../../redux/user/user.actions";
 import { verifyUserCredentials } from "../../firebase/firebase.utils";
+import EditNameForm from "./edit-name.component";
+import EditEmailForm from "./edit-email.component";
+import EditPasswordForm from "./edit-password.component";
 
 const EditAccountForm = ({ data }) => {
   const currentUser = useSelector(selectCurrentUser);
@@ -73,8 +74,6 @@ const EditAccountForm = ({ data }) => {
       currentPassword,
       newPassword,
     }) => {
-      console.log(password);
-
       if (data === "name") {
         currentUser.displayName = name;
         dispatch(updateUserDetail(currentUser, history));
@@ -98,143 +97,12 @@ const EditAccountForm = ({ data }) => {
     },
   });
 
-  if (data === "name") {
-    return (
-      <Form noValidate onSubmit={formik.handleSubmit}>
-        <span className="subtitle">
-          If you want to change the name associated with your IMDb account, you
-          may do so below. Be sure to click the <strong>Save Changes </strong>
-          button when you are done.
-        </span>
-        <Form.Group controlId="formBasicName">
-          <Form.Label>Full Name</Form.Label>
-          <Form.Control
-            type="text"
-            {...formik.getFieldProps("name")}
-            isInvalid={formik.touched.name && formik.errors.name}
-          />
-          <Form.Control.Feedback type="invalid">
-            {formik.errors.name}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Save changes
-        </Button>
-      </Form>
-    );
-  }
+  if (data === "name") return <EditNameForm formik={formik} />;
 
-  if (data === "email") {
-    return (
-      <Form noValidate onSubmit={formik.handleSubmit}>
-        <span className="subtitle">
-          Use the form below to change the e-mail address for your IMDb account.
-          Use the new address next time you log in. If you don’t know your
-          password you can reset it{" "}
-          {<Link to="/account/reset-password">here</Link>}.
-        </span>
-        <Form.Group controlId="formBasicOldEmail">
-          <Form.Label>Old email address:</Form.Label>
-          <Form.Control
-            type="email"
-            plaintext
-            readOnly
-            defaultValue={currentUser.email}
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>New email address:</Form.Label>
-          <Form.Control
-            type="email"
-            {...formik.getFieldProps("email")}
-            isInvalid={formik.touched.email && formik.errors.email}
-          />
-          <Form.Control.Feedback type="invalid">
-            {formik.errors.email}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="formBasicEmailVerify">
-          <Form.Label>Re-enter email address:</Form.Label>
-          <Form.Control
-            type="email"
-            {...formik.getFieldProps("emailVerify")}
-            isInvalid={formik.touched.emailVerify && formik.errors.emailVerify}
-          />
-          <Form.Control.Feedback type="invalid">
-            {formik.errors.emailVerify}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password:</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            {...formik.getFieldProps("password")}
-            isInvalid={formik.touched.password && formik.errors.password}
-          />
-          <Form.Control.Feedback type="invalid">
-            {formik.errors.password}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Save changes
-        </Button>
-      </Form>
-    );
-  }
+  if (data === "email")
+    return <EditEmailForm formik={formik} currentUser={currentUser} />;
 
-  if (data === "password") {
-    return (
-      <Form noValidate onSubmit={formik.handleSubmit}>
-        <span className="subtitle">
-          Use the form below to change the password for your IMDb account
-        </span>
-        <Form.Group controlId="formCurrentPassword">
-          <Form.Label>Current password:</Form.Label>
-          <Form.Control
-            type="password"
-            name="currentPassword"
-            {...formik.getFieldProps("currentPassword")}
-            isInvalid={
-              formik.touched.currentPassword && formik.errors.currentPassword
-            }
-          />
-          <Form.Control.Feedback type="invalid">
-            {formik.errors.currentPassword}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="formNewPassword">
-          <Form.Label>New password:</Form.Label>
-          <Form.Control
-            type="password"
-            name="newPassword"
-            {...formik.getFieldProps("newPassword")}
-            isInvalid={formik.touched.newPassword && formik.errors.newPassword}
-          />
-          <Form.Control.Feedback type="invalid">
-            {formik.errors.newPassword}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="formReenterPassword">
-          <Form.Label>Reenter new assword:</Form.Label>
-          <Form.Control
-            type="password"
-            name="reEnterPassword"
-            {...formik.getFieldProps("reEnterPassword")}
-            isInvalid={
-              formik.touched.reEnterPassword && formik.errors.reEnterPassword
-            }
-          />
-          <Form.Control.Feedback type="invalid">
-            {formik.errors.reEnterPassword}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Save changes
-        </Button>
-      </Form>
-    );
-  }
+  if (data === "password") return <EditPasswordForm formik={formik} />;
 };
 
 export default EditAccountForm;
